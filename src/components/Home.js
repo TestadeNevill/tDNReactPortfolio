b6ed7c4b1a0b14ab9e6055eb724992fe131df54e
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-01-20 10:27:00 -0500

    edit home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,11 @@ function Home() {
 
         <div id="home-div">
             <p className="p one-second-animation" id="hi-there" >Hi there! I'm</p>
-            <h1 className="h1 three-second-animation" >Datesta Nickle </h1>
+            <h1 className="h1 three-second-animation" >Testa De Nevill </h1>
             {/* <h1 className="h2 three-second-animation" >Testa de Nevill</h1> */}
             <h2 className="h2 five-second-animation" >I'm a Full Stack Web Developer</h2>
             <Link to='/resume' exact className="p seven-second-animation" id="intro" >
-                <p className="p seven-second-animation" id="intro" >I have experience in Javascript, Express, Node, MySQL, MongoDB, React and more.</p>
+                <p className="p seven-second-animation" id="intro" >I have experience in HTML, CSS, Javascript, Express, Node, MySQL, MongoDB and React.</p>
             </Link>
             <p className="nine-second-animation">This website was created using React.js and CSS.</p>
             <a href={`mailto:dnickle.cet@gmail.com`} className="ten-second-animation" >
